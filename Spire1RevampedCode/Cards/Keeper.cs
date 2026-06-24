d53d518eb22447ba5f71eb66c7b54b4f36163110
Paradoxica <jacklam65536@gmail.com>
2026-06-24 22:13:49 +1000

    changed keeper to be osty attack package enabler, added asset for rogue card

--- Spire1RevampedCode/Cards/Keeper.cs
+++ Spire1RevampedCode/Cards/Keeper.cs
@@ -5,10 +5,10 @@ using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Entities.Creatures;
-using MegaCrit.Sts2.Core.Hooks;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.CardPools;
 using MegaCrit.Sts2.Core.Models.Characters;
+using Spire1Revamped.Spire1RevampedCode.Powers;
 
 #nullable enable
 namespace Spire1Revamped.Spire1RevampedCode.Cards;
@@ -18,28 +18,22 @@ public class Keeper() : Spire1RevampedCard(1,
     CardType.Skill, CardRarity.Ancient,
     TargetType.Self)
 {
-    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(StaticHoverTip.SummonDynamic, (DynamicVar) this.DynamicVars.Summon),HoverTipFactory.Static(StaticHoverTip.Block)];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(StaticHoverTip.SummonDynamic, (DynamicVar) this.DynamicVars.Summon)];
 
     protected override IEnumerable<DynamicVar> CanonicalVars => [
-        (DynamicVar) new SummonVar(7M),
-        new DynamicVar("BlockThreshold", 3M),
-        (DynamicVar) new CalculationBaseVar(7M),
-        (DynamicVar) new CalculationExtraVar(1M),
-        (DynamicVar) new CalculatedVar("CalculatedSummon").WithMultiplier((Func<CardModel, Creature, Decimal>) ((card, target) => Math.Floor((card.Owner.Creature.Block) / card.DynamicVars["BlockThreshold"].BaseValue))),
-        (DynamicVar) new CalculatedVar("CalculatedDisplaySummon").WithMultiplier((Func<CardModel, Creature, Decimal>) ((card, target) => Hook.ModifySummonAmount(card.CombatState,card.Owner, Math.Floor((card.Owner.Creature.Block) / card.DynamicVars["BlockThreshold"].BaseValue),card)))
+        (DynamicVar) new SummonVar(7M)
     ];
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
         Keeper keeper = this;
         await CreatureCmd.TriggerAnim(this.Owner.Creature, Necrobinder.GetSummonAnimIfApplicable(this.Owner.Character), Necrobinder.GetSummonDelayIfApplicable(this.Owner.Character));
-        SummonResult summonResult = await OstyCmd.Summon(choiceContext, this.Owner, ((CalculatedVar) this.DynamicVars["CalculatedSummon"]).Calculate(cardPlay.Target), (AbstractModel) this);
+        SummonResult summonResult = await OstyCmd.Summon(choiceContext, this.Owner, this.DynamicVars.Summon.BaseValue, (AbstractModel) this);
+        RestlessHandPower? restlessHandPower = await PowerCmd.Apply<RestlessHandPower>(choiceContext, this.Owner.Creature, 1, this.Owner.Creature, (CardModel) this);
     }
 
     protected override void OnUpgrade()
     {
         this.DynamicVars.Summon.UpgradeValueBy(3M);
-        this.DynamicVars.CalculationBase.UpgradeValueBy(3M);
-        this.DynamicVars["BlockThreshold"].UpgradeValueBy(-1M);
     }
 }
---
+++ Spire1RevampedCode/Powers/RestlessHandPower.cs
@@ -0,0 +1,29 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+
+namespace Spire1Revamped.Spire1RevampedCode.Powers;
+
+public sealed class RestlessHandPower : Spire1RevampedPower
+{
+  public override PowerType Type => PowerType.Buff;
+  
+  public override PowerStackType StackType => PowerStackType.Counter;
+  
+  public override int ModifyCardPlayCount(CardModel card, Creature? target, int playCount)
+  {
+    return card.Owner.Creature != this.Owner || card.Type != CardType.Attack || !card.Tags.Contains<CardTag>(CardTag.OstyAttack) ? playCount : playCount + 1;
+  }
+  
+  public override async Task AfterPlayerTurnStartLate(PlayerChoiceContext choiceContext, Player player)
+  {
+    RestlessHandPower restlessHandPower = this;
+    if (player != restlessHandPower.Owner.Player)
+      return;
+    await PowerCmd.Decrement((PowerModel) restlessHandPower);
+  }
+}
