67ae9fa92598df94b9a3e4c2c2a578ea1b3bd4a5
Paradoxica <jacklam65536@gmail.com>
2026-09-08 11:20:59 +1000

    removed redundant directives leftover from removing function casts

--- Spire1RevampedCode/Cards/Fission.cs
+++ Spire1RevampedCode/Cards/Fission.cs
@@ -6,7 +6,6 @@ using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.CardPools;
-using MegaCrit.Sts2.Core.Entities.Creatures;
 
 namespace Spire1Revamped.Spire1RevampedCode.Cards;
 
--- Spire1RevampedCode/Cards/GrimReaper.cs
+++ Spire1RevampedCode/Cards/GrimReaper.cs
@@ -1,7 +1,6 @@
 using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
-using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.CardPools;
