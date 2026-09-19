7450be69c940c7529f21865d8154ac3b17281b74
Paradoxica <jacklam65536@gmail.com>
2026-09-19 13:45:40 +1000

    Rewrote Rogue to be more generically useful by making it into Transfigure-lite with Discarding as the "drawback", Rewrote Vitality to have a meaningful synergy with Replay.

--- Spire1RevampedCode/Cards/Rogue.cs
+++ Spire1RevampedCode/Cards/Rogue.cs
@@ -1,13 +1,10 @@
+using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
-using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.ValueProps;
-using BaseLib.Utils;
 using MegaCrit.Sts2.Core.CardSelection;
-using MegaCrit.Sts2.Core.Entities.Creatures;
-using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.CardPools;
 
 namespace Spire1Revamped.Spire1RevampedCode.Cards;
@@ -15,34 +12,21 @@ namespace Spire1Revamped.Spire1RevampedCode.Cards;
 [Pool(typeof(SilentCardPool))]
 public class Rogue() : Spire1RevampedCard(1, CardType.Skill, CardRarity.Ancient, TargetType.Self)
 {
-  protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(15M, ValueProp.Move)];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(15M, ValueProp.Move)];
 
-  protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromKeyword(CardKeyword.Sly)];
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
+        var card = (await CardSelectCmd.FromHandForDiscard(choiceContext, Owner, new CardSelectorPrefs(CardSelectorPrefs.DiscardSelectionPrompt, 1), null, this)).FirstOrDefault();
 
-  private static readonly SpireField<CardModel, bool> TemporaryReplay = new(() => false);
+        if (card is null) return;
 
-  protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
-  {
-    await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
-    var card = (await CardSelectCmd.FromHandForDiscard(choiceContext, Owner, new CardSelectorPrefs(CardSelectorPrefs.DiscardSelectionPrompt, 1), null, this)).FirstOrDefault();
-    if (card is null) return;
-    if (card._hasSingleTurnSly || card.Keywords.Contains(CardKeyword.Sly)) TemporaryReplay[card] = true;
-    await CardCmd.Discard(choiceContext, card);
-  }
+        ++card.BaseReplayCount;
+        await CardCmd.Discard(choiceContext, card);
+    }
 
-  public override int ModifyCardPlayCount(CardModel card, Creature? target, int playCount)
-  {
-    return card.Owner != Owner || !TemporaryReplay.Get(card) ? playCount : playCount + 1;
-  }
-
-  public override Task AfterCardPlayed(PlayerChoiceContext choiceContext, CardPlay cardPlay)
-  {
-    if (TemporaryReplay.Get(cardPlay.Card)) TemporaryReplay[cardPlay.Card] = false;
-    return Task.CompletedTask;
-  }
-
-  protected override void OnUpgrade()
-  {
-    DynamicVars.Block.UpgradeValueBy(5M);
-  }
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Block.UpgradeValueBy(5M);
+    }
 }
--- Spire1RevampedCode/Cards/Vitality.cs
+++ Spire1RevampedCode/Cards/Vitality.cs
@@ -1,32 +1,35 @@
 using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
-using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.CardPools;
-using MegaCrit.Sts2.Core.Models.Monsters;
-using MegaCrit.Sts2.Core.ValueProps;
 
 namespace Spire1Revamped.Spire1RevampedCode.Cards;
 
 [Pool(typeof(NecrobinderCardPool))]
-
 public class Vitality() : Spire1RevampedCard(1, CardType.Skill, CardRarity.Common, TargetType.Self)
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(2M, ValueProp.Unblockable | ValueProp.Unpowered | ValueProp.Move)];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new SummonVar(3M), new ("HPThreshold",9M)];
 
-    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Retain,CardKeyword.Exhaust];
-
-    protected override bool ShouldGlowRedInternal => Owner.IsOstyMissing;
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Retain, CardKeyword.Exhaust];
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        if (Osty.CheckMissingWithAnim(Owner) || Owner.Osty!.MaxHp == Owner.Osty.CurrentHp) return;
-        var healAmount = Owner.Osty.MaxHp - Owner.Osty.CurrentHp;
-        await CreatureCmd.Heal(cardPlay.Card.Owner.Osty!, healAmount);
-        await CreatureCmd.Damage(choiceContext, CombatState!.HittableEnemies, healAmount*DynamicVars.Damage.BaseValue, ValueProp.Unblockable | ValueProp.Unpowered | ValueProp.Move, Owner.Creature);
+        var missingHp = Owner.Creature.MaxHp - Owner.Creature.CurrentHp;
+
+        var interval = (int)DynamicVars["HPThreshold"].BaseValue;
+        var triggers = missingHp / interval;
+
+        for (var i = 0; i < triggers; ++i)
+        {
+            await OstyCmd.Summon(choiceContext, Owner, DynamicVars.Summon.BaseValue, this);
+        }
     }
 
-    protected override void OnUpgrade() => DynamicVars.Damage.UpgradeValueBy(1M);
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Summon.UpgradeValueBy(-2M);
+        DynamicVars["HPThreshold"].UpgradeValueBy(-6M);
+    }
 }
